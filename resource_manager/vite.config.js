0e5cbd09c495c10e9e25195498072a7ab273ef34
fariaass <farias.lucasteixeira@gmail.com>
2025-11-26 14:13:37 -0300

    feat: resource manager

--- resource_manager/vite.config.js
+++ resource_manager/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 4000,
-    allowedHosts: ['maritime-flow.souzavinicius.com.br'],
+    allowedHosts: ['maritime-flow.souzavinicius.com'],
   },
 })
